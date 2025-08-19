e3d92ffa95c349a24a05635ab9e6807c24ea7d59
Mahmoud Khalifa <m7mkf25@gmail.com>
2025-08-20 00:34:57 +0300

    task 2

--- day5/day5/src/index.js
+++ day5/day5/src/index.js
@@ -10,46 +10,37 @@
 // If the user wants to continue, the application will repeat the process.
 // If the user does not want to continue, the application will exit.
 
-const math = require("../lib/math")
+const math = require("../lib/math");
 
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('please enter operation (add, sub, mult, divide): ', (op) => {
-    rl.question('enter first number: ', (a) => {
-        rl.question('enter second number: ', (b) => {
-            a = Number(a)
-            b = Number(b)
+// rl.question("please enter operation (add, sub, mult, divide): ", (op) => {
+//   rl.question("enter first number: ", (a) => {
+//     rl.question("enter second number: ", (b) => {
+//       a = Number(a);
+//       b = Number(b);
 
-            let result;
-            if (op === 'add') {
-                result = math.add(a, b)
-            }
-            else if (op === 'sub') {
-                result = math.subtract(a, b)
-            }
-            else if (op === 'mult') {
-                result = math.multiply(a, b)
-            }
-            else if (op === 'divide') {
-                result = math.divide(a, b)
-            }
-            else {
-                console.log("Invalid operation!");
-            }
-            
-            console.log("Result:", result);
-            rl.close();
-     })
-        })
-})
+//       let result;
+//       if (op === "add") {
+//         result = math.add(a, b);
+//       } else if (op === "sub") {
+//         result = math.subtract(a, b);
+//       } else if (op === "mult") {
+//         result = math.multiply(a, b);
+//       } else if (op === "divide") {
+//         result = math.divide(a, b);
+//       } else {
+//         console.log("Invalid operation!");
+//       }
 
-
-
-
-
+//       console.log("Result:", result);
+//       rl.close();
+//     });
+//   });
+// });
 
 // TASK 2 (Bouns 50 points):
 // Make a guessing game that asks the user to guess a number between 0 and 50.
@@ -58,6 +49,80 @@ rl.question('please enter operation (add, sub, mult, divide): ', (op) => {
 // The user has 5 attempts to guess the number. if the attempt is wrong, the application will print "Try again 🤔" to the console.
 // If the user does not guess the number correctly 5 times, the application will print "You lost the game!! try again 🤔" to the console.
 // If the user guesses the number correctly, the application will print "You won the game!! congrats 🥳🥳" to the console.
+
+
+let secret = math.randomTo50(); 
+console.log( 'the secret : ',secret);
+let attempts = 5;
+
+console.log(" Guess a number between 0 and 50. You have 5 attempts!");
+
+rl.question(`Enter your guess (Attempts ${attempts}): `, (g1) => {
+  g1 = Number(g1);
+  if (g1 === secret) 
+  {
+    console.log("You won the game!! congrats");
+    rl.close();
+  } 
+  else 
+  {
+    attempts--;
+    console.log("Try again ");
+    rl.question(`Enter your guess (Attempts ${attempts}): `, (g2) => {
+      g2 = Number(g2);
+      if (g2 === secret) {
+        console.log("You won the game!! congrats ");
+        rl.close();
+      }
+       else
+         {
+        attempts--;
+        console.log("Try again ");
+        rl.question(`Enter your guess (Attempts ${attempts}): `, (g3) => {
+          g3 = Number(g3);
+          if (g3 === secret) 
+          {
+            console.log(" You won the game!! congrats ");
+            rl.close();
+          } 
+          else 
+        {
+            attempts--;
+            console.log("Try again ");
+            rl.question(`Enter your guess (Attempts ${attempts}): `,(g4) => {
+                g4 = Number(g4);
+                if (g4 === secret) 
+                {
+                  console.log("🎉 You won the game!! congrats ");
+                  rl.close();
+                } 
+                else 
+                {
+                  attempts--;
+                  console.log("Try again ");
+                  rl.question(`Enter your guess (Attempts ${attempts}): `, (g5) => {
+                      g5 = Number(g5);
+                      if (g5 === secret)
+                      {
+                        console.log(" You won the game!! congrats ");
+                      } 
+                      else 
+                      {
+                        console.log(" You lost the game!! try again ");
+                      }
+                      rl.close();
+                    }
+                  );
+                }
+              }
+            );
+          }
+        });
+      }
+    });
+  }
+});
+
 
 // TASK 3 (Bouns 50 points):
 // Make a function that ask the user the following questions:
